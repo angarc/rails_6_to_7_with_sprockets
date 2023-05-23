978c611f821bdfe585eba60189877fafcec235cd
Angel Garcia <ag4219@columbia.edu>
2023-05-23 16:17:07 -0600

    Fixed gemfile bugs

--- rails_6_to_7_with_sprockets/cli.py
+++ rails_6_to_7_with_sprockets/cli.py
@@ -1,11 +1,14 @@
 import re
-from .file_helpers import replace_line_in_file
+import subprocess
+from .file_helpers import replace_line_in_file, add_to_file
 
 def run_pipeline():
   gemfile_path = "./Gemfile"
 
   update_rails_version(gemfile_path)
   update_bootstrap_version(gemfile_path)
+  add_sprockets_gem(gemfile_path)
+  bundle_update()
 
 def update_rails_version(gemfile_path):
   replace_line_in_file(
@@ -20,3 +23,28 @@ def update_bootstrap_version(gemfile_path):
     re.compile('gem [\'"]bootstrap[\'"]'),
     "gem 'bootstrap', '~> 4.5'\n"
   )
+
+def add_sprockets_gem(gemfile_path):
+  add_to_file(
+    gemfile_path, 
+    re.compile('gem [\'"]sprockets-rails[\'"]'),
+    "gem 'sprockets-rails'\n"
+  )
+
+def bundle_update():
+  p = subprocess.Popen(['bundle', 'update'], bufsize=2048, stdin=subprocess.PIPE)
+  p.wait()
+
+  if p.returncode == 0:
+    print("bundle updated")
+  else:
+    print("bundle update failed")
+
+def bundle_install():
+  p = subprocess.Popen(['bundle', 'install'], bufsize=2048, stdin=subprocess.PIPE)
+  p.wait()
+
+  if p.returncode == 0:
+    print("bundle installed")
+  else:
+    print("bundle install failed")
--- rails_6_to_7_with_sprockets/file_helpers.py
+++ rails_6_to_7_with_sprockets/file_helpers.py
@@ -10,3 +10,19 @@ def replace_line_in_file(file_path, regex, replacement):
   
   with open(file_path, 'w') as f:
     f.writelines(new_lines)
+
+def add_to_file(file_path, regex, new_line):
+  found = False
+  with open(file_path, 'r') as file:
+    file_lines = file.readlines()
+    for line in file_lines:
+      if regex.match(line):
+        found = True
+        break
+
+  if found:
+    print("found")
+    return 
+
+  with open(file_path, 'a') as f:
+    f.write('\n' + new_line + '\n')
